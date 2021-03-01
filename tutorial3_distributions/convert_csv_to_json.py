4bd1dfdc6c62f1ab839a3ab143c5282f8c14df79
boraseo560 <boraseo560@gmail.com>
2021-02-28 23:34:30 -0500

    Updated for 2/22 Assignment

--- tutorial3_distributions/convert_csv_to_json.py
+++ tutorial3_distributions/convert_csv_to_json.py
@@ -1,8 +1,10 @@
 import json
 import csv
 
+# Data source: https://www.kaggle.com/fedesoriano/stroke-prediction-dataset
 with open("D:/Interactive-Data-Vis-Sp2021/tutorial3_distributions/healthcare-dataset-stroke-data.csv", "r", encoding="UTF-8") as data:
     haha = csv.DictReader(data)
     haha_list = list(haha)
+
 with open("D:/Interactive-Data-Vis-Sp2021/tutorial3_distributions/healthcare-dataset-stroke-data.json", "w", encoding="UTF-8") as output:
     json.dump(haha_list, output, indent=4)
