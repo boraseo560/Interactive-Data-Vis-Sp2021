74c4a87948aeec7b9242a144eaa7dbdbad226c3d
boraseo560 <boraseo560@gmail.com>
2021-03-14 19:27:00 -0400

    Updated for 3/8 assignment

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -30,8 +30,8 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("usState.json"),
-  d3.csv("usHeatExtremes.csv", d3.autoType),
+  d3.json("../data/usState.json"),
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
 ]).then(([geojson, otherData]) => {
   // + SET STATE WITH DATA
   state.heat = otherData
@@ -74,26 +74,24 @@ function init() {
     // })
     .attr("d", pathFunction)
 
-  radius = d3.scaleLinear()
-    .domain([d3.min(state.heat, d => d.TempChange), d3.max(state.heat, d => d.TempChange)])
-    .range([1, 10])
+  radius = d3.scaleLinear([0, d3.max(state.heat, d => d.TempChange)], [3, 13])
 
   svg.selectAll("circle.point")
     .data(state.heat)
     // .filter(d => [d.Long, d.Lat])
     // .sort((a, b) => d3.descending(a['Change in 95 percent Days'], b['Change in 95 percent Days'])))
     .join("circle")
-    .attr("r", d => radius(d.TempChange))
+    .attr("r", d => radius(Math.abs(d.TempChange)))
     .attr("stroke", "#ccc")
     .attr("fill", d => {
       if (d.TempChange > 0) return "#ef2d2d";
-      else if (d.TempChange === 0) return "#eec718"
+      else if (d.TempChange === 0) return "#ffa900"
       else return "#0576d0"
 
     })
     .attr("fill-opacity", d => {
       if (d.TempChange > 0) return "0.7"
-      else if (d.TempChange === 0) return "0.6"
+      else if (d.TempChange === 0) return "0.7"
       else return "0.7"
     })
     .attr("transform", d => {
@@ -103,13 +101,14 @@ function init() {
     .on("mousemove", function (event, d) {
       d3.select(this).transition()
         .duration("30")
-        .attr("r", 15)
+        .attr("stroke", "black")
+        .attr("r", 16)
       const { clientX, clientY } = event
       const [long, lat] = projection.invert([clientX, clientY])
 
       state.hover = {
         stateName: d.State,
-        heatChange: [d.TempChange],
+        heatChange: d.TempChange,
         screenPosition: [clientX, clientY],
         mapPosition: [d3.format(".2f")(long), d3.format(".2f")(lat)],
         visible: true
@@ -119,6 +118,7 @@ function init() {
     .on("mouseout", function (event, d) {
       d3.select(this).transition()
         .duration("50")
+        .attr("stroke", "#ccc")
         .attr("r", d => radius(d.TempChange))
       state.hover.visible = false
       draw();
