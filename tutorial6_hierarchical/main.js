f621aca3a4686cde3f96a32e1d3dc444f4676b3b
boraseo560 <boraseo560@gmail.com>
2021-03-21 23:56:42 -0400

    Updated for 3/15 assignment

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -2,7 +2,7 @@
  * CONSTANTS AND GLOBALS
  * */
 const width = window.innerWidth * 0.9,
-  height = window.innerHeight * 0.7,
+  height = window.innerHeight * 0.9,
   margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 let svg;
@@ -12,13 +12,15 @@ let tooltip;
  * APPLICATION STATE
  * */
 let state = {
+  data: null,
+  hover: null
   // + INITIALIZE STATE
 };
 
 /**
  * LOAD DATA
  * */
-d3.json("../../data/flare.json", d3.autotype).then(data => {
+d3.json("../data/flare.json", d3.autotype).then(data => {
   state.data = data;
   init();
 });
@@ -28,6 +30,9 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
  * this will be run *one time* when the data finishes loading in
  * */
 function init() {
+
+  const colorScale = d3.scaleOrdinal(d3.schemeSet3)
+  // console.log(state.data)
   const container = d3.select("#d3-container").style("position", "relative");
 
   svg = container
@@ -36,15 +41,68 @@ function init() {
     .attr("height", height);
 
   // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
+  tooltip = container.append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("top", 0)
+    .style("left", 0)
+    .style("background-color", "white")
 
   // + CREATE YOUR ROOT HIERARCHY NODE
+  const root = d3.hierarchy(state.data)
+    .sum(d => d.value)
+
+  // console.log(state.data)
+  // console.log(root)
 
   // + CREATE YOUR LAYOUT GENERATOR
+  const treeLayout = d3.treemap()
+    .size([width, height])
+    .padding(1)
 
   // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
+  treeLayout(root)
 
   // + CREATE YOUR GRAPHICAL ELEMENTS
+  const leaves = root.leaves()
+  // console.log(leaves)
 
+  const leafGroup = svg.selectAll("rect")
+    .data(leaves)
+    // .join("rect")
+    .join("g")
+    .attr("transform", d => `translate(${d.x0}, ${d.y0})`)
+
+  leafGroup.append("rect")
+    .attr("fill", d => {
+      const level1Ancestor = d.ancestors().find(a => a.depth === 1)
+      return colorScale(level1Ancestor.data.name)
+    })
+    .attr("stroke", "black")
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+
+  // leafGroup.append("text")
+  //   .attr("dy", "1em")
+  //   .text(d => d.data.name)
+  //   .attr("font-size", "8")
+
+  leafGroup.on("mouseover", (event, d) => {
+    state.hover = {
+      position: [d.x0, d.y0],
+      name: d.data.name,
+      value: d.data.value,
+      ancestorsPath: d.ancestors()
+        .reverse()
+        .map(d => d.data.name)
+        .join("/")
+    }
+    draw()
+  })
+    .on("mouseleave", () => {
+      state.hover = null
+      draw();
+    })
   draw(); // calls the draw function
 }
 
@@ -54,4 +112,21 @@ function init() {
  * */
 function draw() {
   // + UPDATE TOOLTIP
+
+  if (state.hover) {
+    tooltip
+      .html(
+        `
+        <div>Name: ${state.hover.name} </div>
+        <div>Value: ${state.hover.value}</div>
+        <div>Hierarchy Path: ${state.hover.ancestorsPath}</div>
+      `
+      )
+      .style("font-size", "8px")
+      .transition()
+      .duration(180)
+      .style("transform", `translate(${state.hover.position[0]}px, ${state.hover.position[1]}px )`)
+
+  }
+  tooltip.classed("visible", state.hover)
 }
---
+++ tutorial6_hierarchical/pack.js
@@ -0,0 +1,136 @@
+
+let svgPack;
+let tooltipPack;
+
+/**
+* APPLICATION STATE
+* */
+let statePack = {
+    data: null,
+    hover: null
+    // + INITIALIZE STATE
+};
+
+/**
+* LOAD DATA
+* */
+d3.json("../data/flare.json", d3.autotype).then(data => {
+    statePack.data = data;
+    initPack();
+});
+
+/**
+* INITIALIZING FUNCTION
+* this will be run *one time* when the data finishes loading in
+* */
+function initPack() {
+
+    // const color = d3.scaleOrdinal(d3.schemeSet3)
+    const color = d3.scaleSequential([0, 8], d3.interpolateYlGnBu)
+    // console.log(state.data)
+    const containerPack = d3.select("#pack-circle").style("position", "relative");
+
+    svgPack = containerPack
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("text-anchor", "middle")
+
+    // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
+    tooltipPack = containerPack.append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("top", 0)
+        .style("left", 0)
+        .style("background-color", "white")
+
+    // + CREATE YOUR ROOT HIERARCHY NODE 
+    const rootPack = d3.hierarchy(statePack.data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value)
+
+    const circlePack = d3.pack()
+        .size([width - 2, height - 2])
+        .padding(3)
+    // console.log(state.data)
+    // console.log(root)
+
+    // + CREATE YOUR LAYOUT GENERATOR
+    // const treeLayout = d3.treemap()
+    //     .size([width, height])
+    //     .padding(1)
+
+    // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
+    // treeLayout(root)
+    circlePack(rootPack)
+    const nodePack = rootPack.descendants()
+
+
+    // + CREATE YOUR GRAPHICAL ELEMENTS
+    // const leavesPack = rootPack.leaves()
+    // console.log(leaves)
+
+    const packGroup = svgPack.selectAll("g")
+        .data(nodePack)
+        .join("g")
+        .attr("transform", d => `translate(${d.x},${d.y})`)
+
+    packGroup.append("circle")
+        .attr("r", d => d.r)
+        .attr("fill", d => color(d.height))
+        // .attr("fill", d => {
+        //     const pack1Ancestor = d.ancestors().find(a => a.depth === 1)
+        //     return color(pack1Ancestor.data.name)
+        // })
+        .attr("stroke", "black")
+        .attr("width", d => d.x)
+        .attr("height", d => d.y)
+
+    // leafGroup.append("text")
+    //   .attr("dy", "1em")
+    //   .text(d => d.data.name)
+    //   .attr("font-size", "8")
+
+    packGroup.on("mouseover", (event, d) => {
+        statePack.hover = {
+            position: [d.x, d.y],
+            name: d.data.name,
+            value: d.data.value,
+            ancestorsPath: d.ancestors()
+                .reverse()
+                .map(d => d.data.name)
+                .join("/")
+        }
+        drawPack()
+    })
+        .on("mouseleave", () => {
+            statePack.hover = null
+            drawPack();
+        })
+    drawPack(); // calls the draw function
+}
+
+/**
+* DRAW FUNCTION
+* we call this everytime there is an update to the data/state
+* */
+function drawPack() {
+    // + UPDATE TOOLTIP
+
+    if (statePack.hover) {
+        tooltipPack
+            .html(
+                `
+       <div>Name: ${statePack.hover.name} </div>
+       <div>Value: ${statePack.hover.value} </div>
+       <div>Hierarchy Path: ${statePack.hover.ancestorsPath} </div>
+     `
+            )
+            .style("font-size", "8px")
+            .transition()
+            .duration(200)
+            .style("transform", `translate(${statePack.hover.position[0]}px, ${statePack.hover.position[1]}px )`)
+
+    }
+    tooltipPack.classed("visible", statePack.hover)
+}
